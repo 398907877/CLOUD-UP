079ff7011f281d507f9d38ccb5a46a4a1c8a6186
zjy <zjy9044@163.com>
2016-12-21 00:02:45 +0800

    1

--- bet/src/org/jeecgframework/web/bet/controller/BetController.java
+++ bet/src/org/jeecgframework/web/bet/controller/BetController.java
@@ -1,8 +1,3 @@
-/*
- * Copyright (c) 2005, 2016, EVECOM Technology Co.,Ltd. All rights reserved.
- * EVECOM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- *
- */
 package org.jeecgframework.web.bet.controller;
 
 import java.text.SimpleDateFormat;
